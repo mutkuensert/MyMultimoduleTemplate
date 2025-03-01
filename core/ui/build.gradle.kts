6f81a8cad3365ef85503b2ee76846ef3af01050b
M. Utku Ensert <mutkuensert@hotmail.com>
2025-03-02 00:18:04 +0300

    refactor: remove compose plugin and add compose function

--- core/ui/build.gradle.kts
+++ core/ui/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("base-feature")
-    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
@@ -8,17 +7,5 @@ android {
 }
 
 dependencies {
-    implementation(platform(Libs.androidxComposeBom))
-    androidTestImplementation(Libs.androidxComposeBom)
-    implementation(Libs.androidxComposeRuntime)
-
-    // Core Android dependencies
-
-
-    // Compose
-    implementation(Libs.androidxComposeUi)
-    implementation(Libs.androidxComposeUiToolingPreview)
-    implementation(Libs.androidxComposeMaterial3)
-    // Tooling
-    debugImplementation(Libs.androidXComposeUiTooling)
+    compose()
 }
